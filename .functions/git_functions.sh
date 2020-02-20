a6d5454b0b6317d3d3f53e0ebfb9b4d65a383db1
Rafael Dutra <raffaeldutra@gmail.com>
2020-02-19 22:38:29 -0300

    One new function called git_general_config

--- .functions/git_functions.sh
+++ .functions/git_functions.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 git_change_commiter() {
-cat << EOT
-git commit --amend --author="Rafael Dutra <raffaeldutra@gmail.com>"
-EOT
+    echo "git commit --amend --author='Rafael Dutra <raffaeldutra@gmail.com>'"
 }
 
 git_personal_config() {
 cat << EOT
 git config user.email "raffaeldutra@gmail.com"
 git config user.name "Rafael Dutra"
+EOT
+}
+
+git_general_config() {
+cat << EOT
+git config user.email ""
+git config user.name ""
 EOT
 }
