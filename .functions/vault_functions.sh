cfa2ec5ac046c0ccd610ca82420375a2cdf14b3a
Rafael Dutra <raffaeldutra@gmail.com>
2020-03-17 17:17:27 -0300

    Vault functions

--- .functions/vault_functions.sh
+++ .functions/vault_functions.sh
@@ -1,9 +1,22 @@
 vault_address() {
-    source ${HOME}/.env
-    echo "Connecting in: ${address}"
+    if [ ! -e ~/dotfiles/.env.sh ]; then
+        echo "~/dotfiles/.env.sh not found"
+
+        exit 1
+    fi
+
+    source ~/dotfiles/.env.sh ${1}
 }
 
 vault_login() {
+    if [ "x" = "x${1}" ]; then
+        echo "First parameter must be an environment <prd/uat/dev>"
+
+        return 1
+    fi
+
     vault_address ${1}
+
     vault login -method=ldap username="${LDAP_USERNAME}"
+    echo "Connecting in: ${VAULT_ADDR}"
 }
